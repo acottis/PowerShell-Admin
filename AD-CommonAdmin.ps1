64e00143a66233130d98b07c2d24f06ebbfbc240
Adam Cottis <Adam.Cottis@centralscotland-vjb.gov.uk>
2020-07-16 16:06:26 +0100

    Supports Reset and Unlock

---
+++ AD-CommonAdmin.ps1
@@ -0,0 +1,35 @@
+Import-Module -Name ActiveDirectory -Function Set-ADAccountPassword, Set-ADuser, Unlock-ADAccount
+
+. ./Secrets.ps1
+
+Write-host "Functions: Reset-Password, Unlock-ADAaccount are available" -ForegroundColor Green
+
+function Reset-Password{
+
+    param (
+        [string]$identity = $(Read-Host "Identity"),
+        [string]$password = $password
+    )
+
+    $ADuser =  Get-ADUser $identity
+        If($ADuser) 
+        { 
+            try{
+                Set-ADAccountPassword $identity -reset -NewPassword (ConvertTo-SecureString -AsPlainText $password -Force)
+                Write-Host "Password Changed to $password" -ForegroundColor Green 
+            }
+            catch{
+                Write-Host "Error:"$_.Exception.Message  -ForegroundColor Red
+                return
+            }
+
+            $NeverExpires = Get-ADUser -identity $ADuser -Properties PasswordNeverExpires
+            if(-not $NeverExpires.PasswordNeverExpires){
+                Set-ADUser $identity -ChangePasswordAtLogon $true
+            }
+            else{
+                Write-Host "Never Expire is set to true so they will have manually reset their password" -ForegroundColor Green 
+            }
+          
+        }
+}
